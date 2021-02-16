eb68cfb438602ab93a2f25a1413457ae7a8f8161
ricardo <ricardojorgexavier2003@gmail.com>
2021-02-16 12:28:23 +0000

    1

--- app.py
+++ app.py
@@ -1,83 +1,13 @@
 from flask import Flask, render_template, request
-import psycopg2
+from user import user
+
 
 app = Flask(__name__)
-
-
-def herokudb():
-    Host = 'ec2-54-75-235-28.eu-west-1.compute.amazonaws.com'
-    Database = 'd2i8vmdqr6hqck'
-    User = 'dlopinbemujdnz'
-    Password = 'c2f51674e9042bcd8985d5040cfe270542eec1ed42625e1e7fde56274000d210'
-    return psycopg2.connect(host=Host, database=Database, user=User, password=Password, sslmode='require')
-
-
-def gravar(v1, v2, v3):
-    ficheiro = herokudb()
-    db = ficheiro.cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
-    db.execute("INSERT INTO usr VALUES (%s, %s, %s)", (v1, v2, code(v3)))
-    ficheiro.commit()
-    ficheiro.close()
-
-
-def existe(v1):
-    try:
-        ficheiro = herokudb()
-        db = ficheiro.cursor()
-        db.execute("SELECT * FROM usr WHERE nome = %s", (v1,))
-        valor = db.fetchone()
-        ficheiro.close()
-    except:
-        valor = None
-    return valor
-
-
-def log(v1, v2):
-    ficheiro = herokudb()
-    db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE nome = %s and passe = %s", (v1, code(v2),))
-    valor = db.fetchone()
-    ficheiro.close()
-    return valor
-
-
-def alterar(v1, v2):
-    ficheiro = herokudb()
-    db = ficheiro.cursor()
-    db.execute("UPDATE usr SET passe = %s WHERE nome = %s", (code(v2), v1))
-    ficheiro.commit()
-    ficheiro.close()
-
-
-def apaga(v1):
-    ficheiro = herokudb()
-    db = ficheiro.cursor()
-    db.execute("DELETE FROM usr WHERE nome = %s", (v1,))
-    ficheiro.commit()
-    ficheiro.close()
-
-def code(passe):
-    import hashlib
-    return hashlib.sha3_256(passe.encode()).hexdigest()
-
-
-
-
-def lista():
-    try:
-        ficheiro = herokudb()
-        db = ficheiro.cursor()
-        db.execute("select * from usr ORDER BY nome")
-        valor = db.fetchall()
-        ficheiro.close()
-    except:
-        valor = None
-    return valor
+db = user()
 
 @app.route('/tabela')
 def tabela():
-    dados = lista()
+    dados = db.lista()
     return render_template('tabela.html', tabela=dados, max=len(dados))
 
 @app.route('/registo', methods=['GET', 'POST'])
@@ -88,12 +18,12 @@ def route():
         v2 = request.form['email']
         v3 = request.form['passe']
         v4 = request.form['cpasse']
-        if existe(v1):
+        if db.existe(v1):
             erro = 'O Utilizador já existe.'
         elif v3 != v4:
             erro = 'A palavra passe não coincide.'
         else:
-            gravar(v1, v2, v3)
+            db.gravar(v1, v2, v3)
     return render_template('registo.html', erro=erro)
 
 
@@ -108,9 +38,9 @@ def login():
     if request.method == 'POST':
         v1 = request.form['utilizador']
         v2 = request.form['passe']
-        if not existe(v1):
+        if not db.existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not log(v1, v2):
+        elif not db.log(v1, v2):
             erro = 'A palavra passe está errada.'
         else:
             erro = 'Bem-Vindo.'
@@ -123,12 +53,12 @@ def apagar():
     if request.method == 'POST':
         v1 = request.form['utilizador']
         v2 = request.form['passe']
-        if not existe(v1):
+        if not db.existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not log(v1, v2):
+        elif not db.log(v1, v2):
             erro = 'A palavra passe está errada.'
         else:
-            apaga(v1)
+            db.apaga(v1)
             erro = 'Conta Eliminada com Sucesso.'
     return render_template('apagar.html', erro=erro)
 
@@ -141,14 +71,14 @@ def newpasse():
         v0 = request.form['apasse']
         v2 = request.form['passe']
         v3 = request.form['cpasse']
-        if not existe(v1):
+        if not db.existe(v1):
             erro = 'O Utilizador não existe.'
-        elif not log(v1, v0):
+        elif not db.log(v1, v0):
             erro = 'A palavra passe está errada.'
         elif v2 != v3:
             erro = 'A palavra passe não coincide.'
         else:
-            alterar(v1, v2)
+            db.alterar(v1, v2)
     return render_template('newpasse.html', erro=erro)
 
 
---
+++ db.py
@@ -0,0 +1,10 @@
+class Database:
+
+
+   def __init__( self ):
+
+
+       self.Host = 'ec2-54-155-87-214.eu-west-1.compute.amazonaws.com'
+       self.Database = 'dtri2jftdo5gq'
+       self.User = 'huzjwnqqnoxgne'
+       self.Password = '46f0470cdd34db620327fb8b640178f7d2836c937747dd1580243e2f9b1c9d6f'
